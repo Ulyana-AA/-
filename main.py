fb430cfbd79996aeeb0cb8c0980bea8cb2cd4ee9
Vika2335 <98107387+Vika2335@users.noreply.github.com>
2022-02-23 19:53:31 +0300

    hhnhh

---
+++ main.py
@@ -0,0 +1,37 @@
+id = str(input())
+
+res = worksheet.col_values(1)
+res1 = worksheet.col_values(2)
+res2 = worksheet.get_all_records()
+
+if len(id)>=3 and len(id)<8:
+    tire = id.find("-")
+    if tire == -1:
+        print('Не удалось распознать штрих-код')
+    else:
+        a = id[:tire]
+        b = id[(tire+1):]
+        if len(a) == len(b) == 3:
+            if b not in res:
+                print('Неизвестный абонемент')
+            if not a == res1:
+                if not b == res:
+                    print('Внимание, поддельный штрих-код')
+            for i in res2:
+                res3 = worksheet.row_values(i)
+                res4 = worksheet.row_values(i)
+                if b in res3:
+                    if a in res4:
+                        n=i
+                        K = 'E' + n
+                        M = 'F' + n
+                        res5 = worksheet.get('M')
+                        res6 = worksheet.get('K')
+                        if res5 == res6:
+                            print('Лимит посещений по абонементу исчерпан')
+else:
+    print("Не удалось распознать штрих-код")
+
+if __name__ == '__main__':
+    print(a)
+    print(id)
